8b4c69426bada3c967355c471432cbd86e68a1c9
Diana-Vinnitskaya <132234308+Diana-Vinnitskaya@users.noreply.github.com>
2023-08-31 19:46:48 +0300

    1

---
+++ scripts/deploy.py
@@ -0,0 +1,13 @@
+from brownie import MyERC721, network, config, accounts
+
+
+def deploy_contract():
+    account = accounts[0]
+    contract = MyERC721.deploy({"from" : account})
+    print("contract has been deployed successfully to :", contract.address)
+
+    return contract
+
+def main():
+    deploy_contract()
+
